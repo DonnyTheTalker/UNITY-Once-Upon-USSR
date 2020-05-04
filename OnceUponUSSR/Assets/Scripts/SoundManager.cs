b916919649f9c1b368b06fa26bcb21469bfda2a3
DonnyTheTalker <maks.egorov.55@mail.ru>
2020-05-04 22:31:08 +0300

    Main Soundtrack implemented

---
+++ OnceUponUSSR/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource EffectSource;
+    public AudioSource MusicSource;
+    public static SoundManager Instance = null;
+
+    public float LowPitchRange = 0.95f;
+    public float HighPitchRange = 1.05f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySingle(AudioClip clip)
+    {
+        EffectSource.clip = clip;
+        EffectSource.Play();
+    }
+
+    public void RandomizeSfx(params AudioClip[] clips)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(LowPitchRange, HighPitchRange);
+
+        EffectSource.pitch = randomPitch;
+        EffectSource.clip = clips[randomIndex];
+        EffectSource.Play();
+    }
+
+}
