209c435bd77a18143992654b84fdd67343719259
DonnyTheTalker <maks.egorov.55@mail.ru>
2020-05-04 11:53:56 +0300

    Wall script Implemented

---
+++ OnceUponUSSR/Assets/Scripts/Wall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+
+    public Sprite DamageSprite;
+    public int HP = 3;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void TakeDamage(int loss)
+    {
+        _spriteRenderer.sprite = DamageSprite;
+        HP -= loss;
+        if (HP <= 0)
+            gameObject.SetActive(false);
+ 
+    }
+
+
+}
